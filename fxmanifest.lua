09b8d8b11a8dc388bf8dd09b28a7cdbab6103a02
Whlter_White <94198600+Whalter5069@users.noreply.github.com>
2023-08-11 11:51:06 +0600

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,26 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'NK_NETWORK'
+description 'Best Burgershot job scripts for QBCore'
+version = '0.0.3'
+
+client_scripts {
+    'client/client.lua'
+}
+
+server_scripts {
+    'server/server.lua'
+}
+
+dependencies {
+    'qb-target',
+    'qb-menu',
+    'jim-payments'
+}
+
+shared_scripts {
+    'shared/config.lua'
+}
+
+lua54 'yes'
